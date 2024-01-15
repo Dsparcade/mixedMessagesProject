549da2df6cb4aad20b0cea0f2283d6a5a5debe46
Dsparcade <dare.sparcade@gmail.com>
2024-01-15 10:47:47 -0500

    Add song elements object with 3 properties

--- suggester.js
+++ suggester.js
@@ -4,3 +4,9 @@ console.log("\nWelcome to the Karaoke Song Suggester 1.0!\n")
 function generateRandomNumber(num) {
     return Math.floor(Math.random() * num)
 }
+
+const songElements = {
+    genre: ['pop', 'rock', 'R&B', 'country'],
+    decade: ['1960s', '1970s', '1980s', '1990s', '2000s', '2010s'],
+    tempo: ['slow', 'mid-tempo', 'upbeat']
+}
