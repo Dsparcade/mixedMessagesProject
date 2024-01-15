6c15b1e1c26485b7d1d07024c0b49843573f8778
Dsparcade <dare.sparcade@gmail.com>
2024-01-15 11:27:31 -0500

    Add function definition and call to join and print suggestion

--- suggester.js
+++ suggester.js
@@ -31,3 +31,10 @@ for(let prop in songElements) {
             completeSuggestion.push('Cannot make suggestion.');
     }
 }
+
+function joinedSuggestion(suggestionArray) {
+    const joinedSuggestion = completeSuggestion.join(' ');
+    console.log(joined);
+}
+
+joinedSuggestion(completeSuggestion);
